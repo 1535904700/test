71beb9184173b1c3e2948b7b0db11f9b7214c38d
pzj <pzj@qq.com>
2020-07-07 18:54:41 +0800

    new class

--- src/main/java/com/aku/main/Test.java
+++ src/main/java/com/aku/main/Test.java
@@ -2,6 +2,7 @@ package com.aku.main;
 
 public class Test {
     public static void main(String[] args) {
+        String name;
         System.out.println("hello git");
         System.out.println("hello pull");
         System.out.println("ssss");
