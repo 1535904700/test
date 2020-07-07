44033ed31ab12bd27d23c989c5af5cb38b352755
pzj <pzj@qq.com>
2020-07-07 18:55:33 +0800

    new name

--- src/main/java/com/aku/main/Test.java
+++ src/main/java/com/aku/main/Test.java
@@ -3,6 +3,7 @@ package com.aku.main;
 public class Test {
     public static void main(String[] args) {
         String name;
+        String age;
         System.out.println("hello git");
         System.out.println("hello pull");
         System.out.println("ssss");
